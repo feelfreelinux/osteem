37d53dd75e8145b4f119f8523ff80514bb5ef5ed
Filip <rj23@protonmail.com>
2018-05-13 18:49:01 +0200

    Provide posts repository

--- app/src/main/kotlin/io/github/feelfree/osteemt/di/modules/RepositoryModule.kt
+++ app/src/main/kotlin/io/github/feelfree/osteemt/di/modules/RepositoryModule.kt
@@ -2,13 +2,11 @@ package io.github.feelfree.osteemt.di.modules
 
 import dagger.Module
 import dagger.Provides
-import io.github.feelfree.osteemt.api.repository.users.UsersApi
-import io.github.feelfree.osteemt.api.repository.users.UsersRepository
+import io.github.feelfree.osteemt.api.repository.posts.PostsRepository
 import retrofit2.Retrofit
 
 @Module
 class RepositoryModule {
-    // Remember to provide repository with interface
     @Provides
-    fun provide(retrofit: Retrofit) : UsersApi = UsersRepository(retrofit)
+    fun providePostRepository(retrofit: Retrofit) : PostsRepository = PostsRepository(retrofit)
 }
