aaba18c1ab8268792604591ccaefcbc8dd1f2fa6
Filip <rj23@protonmail.com>
2018-05-13 15:37:14 +0200

    Fixed params

--- app/src/main/kotlin/io/github/feelfree/osteemt/api/SteemRequest.kt
+++ app/src/main/kotlin/io/github/feelfree/osteemt/api/SteemRequest.kt
@@ -5,17 +5,18 @@ import com.squareup.moshi.Json
 /**
  * Base class for all AppBase api requests
  * @param method String name of method
- * @param params Moshi annotated object that keeps parameters of method
+ * @param paramObject Moshi annotated object that keeps parameters of method
  */
 class SteemRequest(
     @Json(name = "method")
     val method : String,
-
-    @Json(name = "params")
-    val params : Any
+    val paramObject : Any
 ) {
     @Json(name = "jsonrpc")
     val jsonrpc = "2.0"
+
+    @Json(name = "params")
+    val params = listOf(paramObject)
 
     @Json(name = "id")
     val id = 1
